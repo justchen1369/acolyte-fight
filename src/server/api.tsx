5dc3b3c7cf7309fd667efb2a22ec6a03c347a8a4
Ray Hidayat <ray.hidayat@gmail.com>
2018-07-01 17:46:19 +1200

    Typings for express and socket.io

---
+++ src/server/api.tsx
@@ -0,0 +1,4 @@
+import * as express from 'express';
+
+export function attachApi(app: express.Application) {
+}
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,10 +1,13 @@
+import httpLib from 'http';
+import socketLib from 'socket.io';
+import express from 'express';
 import { attachToSocket } from './connector';
+import { attachApi } from './api';
 import { logger } from './logging';
 
-const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = new httpLib.Server(app);
+const io = socketLib(http);
 
 const program = require('commander');
 program.option('--port <port>', 'Port number');
@@ -12,8 +15,10 @@ program.parse(process.argv);
 
 const port = program.port || process.env.PORT || 7770;
 
-app.use(express.static('./'));
 attachToSocket(io);
+attachApi(app);
+app.use(express.static('./'));
+
 http.listen(port, function() {
 	logger.info("Started listening on port " + port);
 });
