1e1baf31ddb9f45a4d68ca8d2d7ee23decfb2a73
Ray Hidayat <ray.hidayat@gmail.com>
2018-07-09 22:25:51 +1200

    Reducing the charge time of kamehameha

--- src/game/constants.tsx
+++ src/game/constants.tsx
@@ -292,7 +292,7 @@ export namespace Spells {
 		icon: "glowingHands",
 
 		maxAngleDiff: 0.01 * 2 * Math.PI,
-		chargeTicks: 1 * TicksPerSecond,
+		chargeTicks: 0.75 * TicksPerSecond,
 		cooldown: 20 * TicksPerSecond,
 
 		knockbackCancel: true,
