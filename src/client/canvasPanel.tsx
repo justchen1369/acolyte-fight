e987279a63c14ee1c03f7151a3761725d493f888
Ray Hidayat <ray.hidayat@gmail.com>
2018-07-22 15:16:59 +1200

    Adding support for all keys

--- src/client/canvasPanel.tsx
+++ src/client/canvasPanel.tsx
@@ -236,14 +236,51 @@ export class CanvasPanel extends React.Component<Props, State> {
 
     private readKey(e: KeyboardEvent) {
         switch (e.code) {
-            case 'KeyQ': return 'q';
-            case 'KeyW': return 'w';
-            case 'KeyE': return 'e';
-            case 'KeyR': return 'r';
-            case 'KeyA': return 'a';
-            case 'KeyS': return 's';
-            case 'KeyD': return 'd';
-            case 'KeyF': return 'f';
+            case "KeyQ": return 'q';
+            case "KeyW": return 'w';
+            case "KeyE": return 'e';
+            case "KeyR": return 'r';
+            case "KeyT": return 't';
+            case "KeyY": return 'y';
+            case "KeyU": return 'u';
+            case "KeyI": return 'i';
+            case "KeyO": return 'o';
+            case "KeyP": return 'p';
+            case "KeyA": return 'a';
+            case "KeyS": return 's';
+            case "KeyD": return 'd';
+            case "KeyF": return 'f';
+            case "KeyG": return 'g';
+            case "KeyH": return 'h';
+            case "KeyJ": return 'j';
+            case "KeyK": return 'k';
+            case "KeyL": return 'l';
+            case "KeyZ": return 'z';
+            case "KeyX": return 'x';
+            case "KeyC": return 'c';
+            case "KeyV": return 'v';
+            case "KeyB": return 'b';
+            case "KeyN": return 'n';
+            case "KeyM": return 'm';
+            case "Digit0": return '0';
+            case "Digit1": return '1';
+            case "Digit2": return '2';
+            case "Digit3": return '3';
+            case "Digit4": return '4';
+            case "Digit5": return '5';
+            case "Digit6": return '6';
+            case "Digit7": return '7';
+            case "Digit8": return '8';
+            case "Digit9": return '9';
+            case "Minus": return '-';
+            case "Equal": return '=';
+            case "BracketLeft": return '[';
+            case "BracketRight": return ']';
+            case "Semicolon": return ';';
+            case "Quote": return "'";
+            case "Comma": return ',';
+            case "Period": return '.';
+            case "Space": return ' ';
             default: return e.key && e.key.toLowerCase();
         }
     }
