0bad6ea562b36bd224b5637fe792e86befba731b
Ray Hidayat <ray.hidayat@gmail.com>
2018-08-19 15:41:59 +1200

    Faster shrink at start

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -26,7 +26,7 @@ export const World: WorldSettings = {
 
     LavaDamagePerSecond: 15,
     SecondsToShrink: 90,
-    ShrinkPower: 1.375,
+    ShrinkPower: 1.437,
     InitialShieldSeconds: 1.0,
 }
 
