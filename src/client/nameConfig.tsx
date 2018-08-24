6479a583ded7dab83e7797331f3592743d6c293c
Ray Hidayat <ray.hidayat@gmail.com>
2018-08-24 21:55:06 +1200

    Only displaying name change when name has been changed

--- src/client/nameConfig.tsx
+++ src/client/nameConfig.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 interface State {
     name: string;
+    changed: boolean;
     saved: boolean;
 }
 
@@ -18,6 +19,7 @@ export class NameConfig extends React.Component<Props, State> {
         super(props);
         this.state = {
             name: Storage.loadName(),
+            changed: false,
             saved: true,
         }
     }
@@ -25,17 +27,18 @@ export class NameConfig extends React.Component<Props, State> {
         return <div>
             <h1>Your Name</h1>
             <div><input type="text" value={this.state.name} maxLength={PlayerName.MaxPlayerNameLength} onChange={(e) => this.onChange(e)} /></div>
-            <div style={{ marginTop: 8 }}>
+            {this.state.changed && <div style={{ marginTop: 8 }}>
                 {this.state.saved 
                     ? "Your name has been set to " + this.state.name
                     : "Unsaved changes"}
-            </div>
+            </div>}
         </div>;
     }
 
     private onChange(ev: React.ChangeEvent<HTMLInputElement>) {
         this.setState({
             name: PlayerName.sanitizeName(ev.target.value),
+            changed: true,
             saved: false,
         });
         this.saveStateDebounced();
