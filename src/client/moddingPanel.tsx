438c56c27499ff39740750a86548397c017d7646
Ray Hidayat <ray.hidayat@gmail.com>
2018-08-28 21:17:43 +1200

    Removed drag-and-drop help text

--- src/client/moddingPanel.tsx
+++ src/client/moddingPanel.tsx
@@ -48,7 +48,6 @@ export class ModdingPanel extends React.Component<Props, State> {
             <p>Choose a mod file: <input className="file-selector" type="file" onChange={e => this.setState({ selectedFile: e.target.files.item(0) })} /></p>
             <p><div className={this.state.selectedFile ? "btn" : "btn btn-disabled"} onClick={() => this.onSubmit()}>Create Room</div></p>
             {this.state.error && <p className="error">{this.state.error}</p>}
-            <p><b>Tip: </b>you can drag-and-drop a mod file onto this window to quickly create a modded room</p>
         </div>;
     }
 
