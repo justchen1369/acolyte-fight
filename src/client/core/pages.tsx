63d563629d2c04712113f85d2fc92ea67f50f367
Ray Hidayat <ray.hidayat@gmail.com>
2018-08-30 02:30:40 +1200

    Disconnected don't need a notification

--- src/client/core/pages.tsx
+++ src/client/core/pages.tsx
@@ -1,10 +1,7 @@
 import * as s from '../store.model';
 import * as w from '../../game/world.model';
 import * as StoreProvider from '../storeProvider';
-import * as notifications from './notifications';
 import * as url from '../url';
-
-notifications.attachNotificationListener(onNotification);
 
 export function changePage(newPage: string) {
     const store = StoreProvider.getState();
@@ -14,31 +11,4 @@ export function changePage(newPage: string) {
     } else {
         StoreProvider.dispatch({ type: "updatePage", page: newPage });
     }
-}
-
-function updateUrl() {
-    const store = StoreProvider.getState();
-    const current = store.current;
-
-    StoreProvider.dispatch({
-        type: "updateUrl",
-        current: {
-            ...current,
-            gameId: store.world.ui.myGameId,
-            party: store.party ? store.party.id : null,
-        },
-    });
-}
-
-function onNotification(notifs: w.Notification[]) {
-    let urlUpdated = false;
-    notifs.forEach(n => {
-        if (n.type === "disconnected") {
-            urlUpdated = true;
-        }
-    });
-    if (urlUpdated) {
-        updateUrl();
-    }
-
 }
