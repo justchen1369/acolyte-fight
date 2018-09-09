252ce7834d280a9a442e51f808da523cc663ee85
Ray Hidayat <ray.hidayat@gmail.com>
2018-09-09 23:39:19 +1200

    Dash on right-click again

--- src/client/ui/canvasPanel.tsx
+++ src/client/ui/canvasPanel.tsx
@@ -225,7 +225,9 @@ class CanvasPanel extends React.Component<Props, State> {
                         && (p.time - this.previousTouchStart.time) < DoubleTapMilliseconds
                         && vector.distance(p.interfacePoint, this.previousTouchStart.interfacePoint) <= DoubleTapPixels
                     if (isMobile && doubleClick || p.secondaryBtn) {
-                        const spellId = this.keyToSpellId(this.rebind(w.Actions.RightClick));
+                        const spellId =
+                            this.keyToSpellId(this.rebind(w.Actions.RightClick))
+                            || this.keyToSpellId(this.rebind("a")); // Dash spell
                         if (spellId) {
                             sendAction(world.ui.myGameId, world.ui.myHeroId, { type: spellId, target: world.ui.nextTarget });
                         }
