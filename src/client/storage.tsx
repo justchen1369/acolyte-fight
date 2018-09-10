c5f9d45671d61a85d0ca67b168da5921ab59b8b8
Ray Hidayat <ray.hidayat@gmail.com>
2018-09-10 20:21:25 +1200

    Keep 250 games

--- src/client/storage.tsx
+++ src/client/storage.tsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 import { DefaultSettings } from '../game/settings';
 import * as d from './stats.model';
 
-const GamesToKeep = 100;
+const GamesToKeep = 250;
 
 const gameStorage = localForage.createInstance({ name: 'acolyte-fight-games' });
 
