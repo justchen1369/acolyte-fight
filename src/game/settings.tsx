132b21627134991fb35b2e5b6a04182865233ffb
Ray Hidayat <ray.hidayat@gmail.com>
2018-11-28 21:24:08 +1300

    Slower acolyte beam pre-turn rate

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -341,7 +341,7 @@ const kamehameha: Spell = {
     maxAngleDiffInRevs: 0.0001, // Requires a lot of accuracy for long-distance targets
     chargeTicks: 0.75 * TicksPerSecond,
     cooldown: 20 * TicksPerSecond,
-    revsPerTickWhileCharging: 0.002,
+    revsPerTickWhileCharging: 0.001,
     revsPerTickWhileChannelling: 0.00005,
 
     knockbackCancel: true,
