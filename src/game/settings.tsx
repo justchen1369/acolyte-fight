f678d5a999a329c2f47a2aee7953ef33b116b655
Ray Hidayat <ray.hidayat@gmail.com>
2019-01-06 23:42:35 +1300

    Halo back to 5 seconds

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -779,7 +779,7 @@ const halo: Spell = {
     icon: "angelOutfit",
 
     maxAngleDiffInRevs: 0.01,
-    cooldown: 7.5 * TicksPerSecond,
+    cooldown: 5 * TicksPerSecond,
     throttle: true,
 
     projectile: {
