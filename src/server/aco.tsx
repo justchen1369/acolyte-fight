049a88445da581d91853b7aeaf3a2d6a38fe9044
Ray Hidayat <ray.hidayat@gmail.com>
2019-01-16 00:09:31 +1300

    Higher levels can win/lose points faster against lower ranks

--- src/server/aco.tsx
+++ src/server/aco.tsx
@@ -1,13 +1,16 @@
 export class Aco {
     k: number;
+    power: number;
 
-    constructor(k: number) {
+    constructor(k: number, power: number) {
         this.k = k;
+        this.power = power;
     }
 
     adjustment(selfRating: number, otherRating: number, score: number) {
         const diff = selfRating - otherRating;
         const e = 1 / (1 + Math.pow(10, -diff / 400));
-        return this.k * (score - e) * (1 - e);
+        const learnRate = Math.pow(1 - e, this.power);
+        return this.k * (score - e) * learnRate;
     }
 }
