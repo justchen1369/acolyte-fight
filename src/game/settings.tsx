88186eb0c4044f32c44caea7f96dc5e1b4b46404
Ray Hidayat <ray.hidayat@gmail.com>
2019-01-19 00:24:07 +1300

    Forcefield shorter

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -988,7 +988,7 @@ const icewall: Spell = {
     description: "Create a wall that reflects projectiles and blocks other heroes. You can pass through your own forcefield, but other heroes cannot, even if they are using teleport.",
 
     maxRange: 0.25,
-    maxTicks: 1.25 * TicksPerSecond,
+    maxTicks: 1 * TicksPerSecond,
     growthTicks: 5,
     cooldown: 10 * TicksPerSecond,
     throttle: false,
