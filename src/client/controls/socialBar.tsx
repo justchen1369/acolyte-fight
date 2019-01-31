ccb0d867206b3f300c4cd913d208f3669ed59572
Ray Hidayat <ray.hidayat@gmail.com>
2019-01-31 20:49:23 +1300

    Remove Facebook

--- src/client/controls/socialBar.tsx
+++ src/client/controls/socialBar.tsx
@@ -27,8 +27,8 @@ class SocialBar extends React.Component<Props, State> {
     }
 
     private renderSocialBar() {
+        // <a href="http://facebook.com/acolytefight" target="_blank" title="Facebook"><i className="fab fa-facebook" /></a>
         return <div className="social-bar">
-            <a href="http://facebook.com/acolytefight" target="_blank" title="Facebook"><i className="fab fa-facebook" /></a>
             <a href="http://reddit.com/r/acolytefight" target="_blank" title="Reddit"><i className="fab fa-reddit-square" /></a>
             <a href="https://discord.gg/sZvgpZk" target="_blank" title="Chat on Discord!"><i className="fab fa-discord" /></a>
         </div>
