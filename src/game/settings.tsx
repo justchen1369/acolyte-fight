4b1bdcc5b1ce426e632915d5d69e1476dc641c2e
Ray Hidayat <ray.hidayat@gmail.com>
2019-03-02 15:55:33 +1300

    Grapple smaller radius and shorter time

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -880,8 +880,8 @@ const grapple: Spell = {
             targetFactor: 0.1,
             impulsePerTick: 1.0 / TicksPerSecond,
             minDistance: 0.05,
-            maxDistance: 0.25,
-            linkTicks: 3 * TicksPerSecond,
+            maxDistance: 0.15,
+            linkTicks: 2 * TicksPerSecond,
             movementProportion: 2,
 
             render: {
