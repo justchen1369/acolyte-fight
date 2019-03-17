cf1265ff464a8077a6038ffe59c687b40009befe
Ray Hidayat <ray.hidayat@gmail.com>
2019-03-17 22:31:55 +1300

    Start recording when the actual game starts

--- src/client/play/recordPanel.tsx
+++ src/client/play/recordPanel.tsx
@@ -201,7 +201,7 @@ class CanvasPanel extends React.Component<Props, State> {
         const world: w.World = processor.initialWorld(replay);
         console.log("Replay started...", world.ui.myGameId, world.ui.myUserHash);
 
-        while (remaining.length > 0 && !processor.isStartGameTick(remaining[0])) {
+        while (remaining.length > 0 && world.tick < world.startTick) {
             processor.applyTick(remaining.shift(), world);
         }
         this.renderFrame(world, canvasStack);
