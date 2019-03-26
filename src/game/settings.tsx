1eca3689d781a54f0a72cab156704d5c85808071
Ray Hidayat <ray.hidayat@gmail.com>
2019-03-26 18:13:45 +1300

    Freezing breath faster cooldown

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1131,7 +1131,7 @@ const whirlwind: Spell = {
     sound: "whirlwind",
 
     maxAngleDiffInRevs: 0.01,
-    cooldown: 7.5 * TicksPerSecond,
+    cooldown: 5 * TicksPerSecond,
     throttle: true,
     chargeTicks: 0.1 * TicksPerSecond,
 
