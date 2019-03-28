6a729ebb0e41da69647bbf4787836df0bacce191
Ray Hidayat <ray.hidayat@gmail.com>
2019-03-28 17:45:18 +1300

    Revert horcrux cooldown and radius

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1411,7 +1411,7 @@ const horcrux: Spell = {
     icon: "burningEye",
 
     maxAngleDiffInRevs: 0.01,
-    cooldown: 5 * TicksPerSecond,
+    cooldown: 7.5 * TicksPerSecond,
     throttle: true,
 
     projectile: {
@@ -1436,27 +1436,27 @@ const horcrux: Spell = {
         behaviours: [
             {
                 type: "updateCollideWith",
-                trigger: { afterTicks: 45, atCursor: true },
+                trigger: { afterTicks: 60, atCursor: true },
                 collideWith: Categories.All,
             },
             {
                 type: "homing",
                 targetType: "cursor",
-                trigger: { afterTicks: 45, atCursor: true },
+                trigger: { afterTicks: 60, atCursor: true },
                 newSpeed: 0,
                 redirect: true,
             },
             {
                 type: "aura",
-                trigger: { afterTicks: 45, atCursor: true },
-                radius: 0.0325,
+                trigger: { afterTicks: 60, atCursor: true },
+                radius: 0.035,
                 tickInterval: 15,
                 buffs: [
                     {
                         type: "burn",
                         against: Alliances.NotFriendly,
                         hitInterval: 15,
-                        packet: { damage: 3, lifeSteal: 1, minHealth: 1, noHit: true },
+                        packet: { damage: 4, lifeSteal: 1, minHealth: 1, noHit: true },
                         maxTicks: 15,
                         render: {
                             color: "#22ee88",
@@ -1473,11 +1473,11 @@ const horcrux: Spell = {
         sound: "horcrux",
         color: '#22ee88',
         renderers: [
-            { type: "reticule", color: 'rgba(34, 238, 136, 0.1)', radius: 0.0325, minRadius: 0.03, shrinkTicks: 13, grow: true, fade: true, repeat: true },
+            { type: "reticule", color: 'rgba(34, 238, 136, 0.1)', radius: 0.035, minRadius: 0.03, shrinkTicks: 13, grow: true, fade: true, repeat: true },
             { type: "polygon", color: 'rgba(34, 238, 136, 0.5)', numPoints: 5, radiusMultiplier: 2.5, revolutionInterval: 60, ticks: 1 },
             { type: "projectile", ticks: 10, glow: 0.1, smoke: 0.3 },
             { type: "strike", ticks: 10, glow: true, growth: 1.25, numParticles: 5 },
-            { type: "reticule", color: 'rgba(34, 238, 136, 0.5)', radius: 0.0325, minRadius: 0.03, shrinkTicks: 10, startingTicks: 10 },
+            { type: "reticule", color: 'rgba(34, 238, 136, 0.5)', radius: 0.035, minRadius: 0.03, shrinkTicks: 10, startingTicks: 10 },
             {
                 type: "link",
                 color: 'rgba(255, 255, 255, 0.1)',
