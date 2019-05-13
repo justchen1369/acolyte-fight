c8a15e93f03e3e54c55b9abf9781d8d7ae803161
Ray Hidayat <ray.hidayat@gmail.com>
2019-05-13 12:15:22 +1200

    Ensnare shorter

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1158,7 +1158,7 @@ const gravity: Spell = {
 
         gravity: {
             impulsePerTick: 0.001 / TicksPerSecond,
-            ticks: 2.0 * TicksPerSecond,
+            ticks: 1.5 * TicksPerSecond,
             radius: 0.05,
             power: 1,
             render: renderGravity,
@@ -1169,7 +1169,7 @@ const gravity: Spell = {
                 type: "cooldown",
                 against: Alliances.NotFriendly,
                 maxTicks: 1,
-                minCooldown: 0.75 * TicksPerSecond,
+                minCooldown: 0.5 * TicksPerSecond,
             },
         ],
 
