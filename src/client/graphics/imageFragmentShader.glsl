456eaacfff94fb4b459ea9d4ff8c9b7c5260da89
Ray Hidayat <ray.hidayat@gmail.com>
2019-06-05 23:21:22 +1200

    Discard blank pixels

--- src/client/graphics/imageFragmentShader.glsl
+++ src/client/graphics/imageFragmentShader.glsl
@@ -6,5 +6,9 @@ uniform sampler2D u_image;
 varying vec2 v_texCoord;
 
 void main() {
-   gl_FragColor = texture2D(u_image, v_texCoord);
+   vec4 color = texture2D(u_image, v_texCoord);
+   if (color.w == 0.0) {
+      discard;
+   }
+   gl_FragColor = color;
 }
