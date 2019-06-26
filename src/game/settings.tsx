a823c0a102529f8031c686e1e5bca665cc1e4ac8
Ray Hidayat <ray.hidayat@gmail.com>
2019-06-26 18:35:55 +1200

    Repeater 0.4 seconds to max damage

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1080,7 +1080,7 @@ const bouncer: Spell = {
 };
 const repeater: Spell = {
     id: 'repeater',
-    description: "Every time Repeater hits, the cooldown resets and you can shoot it again immediately. Takes 0.25 seconds to grow to full damage, so hit from a distance for maximum damage.",
+    description: "Every time Repeater hits, the cooldown resets and you can shoot it again immediately. Takes 0.4 seconds to grow to full damage, so hit from a distance for maximum damage.",
     action: "projectile",
 
     color: '#00ff00',
@@ -1103,7 +1103,7 @@ const repeater: Spell = {
         expireOn: Categories.All ^ Categories.Shield,
         partialDamage: {
             initialMultiplier: 0.1,
-            ticks: 0.25 * TicksPerSecond,
+            ticks: 0.4 * TicksPerSecond,
         },
 
         buffs: [
