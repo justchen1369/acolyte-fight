6b09350368cb416ec061206d8286aebdf77de590
Ray Hidayat <ray.hidayat@gmail.com>
2019-07-01 22:55:07 +1200

    Revert to high team game chance

--- src/server/games.tsx
+++ src/server/games.tsx
@@ -21,7 +21,7 @@ import { logger } from './logging';
 import { DefaultSettings } from '../game/settings';
 import { required, optional } from './schema';
 
-const TeamGameChance = 0.33;
+const TeamGameChance = 1.0;
 
 const NanoTimer = require('nanotimer');
 const tickTimer = new NanoTimer();
