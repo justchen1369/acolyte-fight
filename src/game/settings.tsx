2bd10ee591f8d8ca6801ff48181b27d1e0656404
Ray Hidayat <ray.hidayat@gmail.com>
2019-07-15 21:56:58 +1200

    Acolyte blast 80 damage (was 50)

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -586,7 +586,7 @@ const blast: Spell = {
         speed: 0.5,
         lifeSteal: 0.2,
         maxTicks: 2 * TicksPerSecond,
-        damage: 50,
+        damage: 80,
 
         detonate: {
             damage: 0,
