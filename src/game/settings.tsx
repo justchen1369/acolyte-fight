95b67e0c8896f069a93fbaf63ab7981b2cb776b5
Ray Hidayat <ray.hidayat@gmail.com>
2019-07-25 18:51:41 +1200

    Revert bouncer knockback

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1277,7 +1277,7 @@ const bouncer: Spell = {
     throttle: true,
 
     projectile: {
-        density: 3.75,
+        density: 1,
         radius: 0.001,
         speed: 1,
         fixedSpeed: true,
