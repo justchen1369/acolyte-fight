7f04649adb434ce091fd1ca4039bc27175381eeb
Ray Hidayat <ray.hidayat@gmail.com>
2019-08-11 01:37:20 +1200

    Saber 1.25 seconds duration

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -2106,7 +2106,7 @@ const saber: Spell = {
 
     icon: "waveStrike",
 
-    maxTicks: 1 * TicksPerSecond,
+    maxTicks: 1.25 * TicksPerSecond,
     channelling: true,
 
     categories: Categories.Shield,
@@ -2166,7 +2166,7 @@ const dualSaber: Spell = {
 
     icon: "waveStrike",
 
-    maxTicks: 1 * TicksPerSecond,
+    maxTicks: 1.25 * TicksPerSecond,
     channelling: true,
 
     categories: Categories.Shield,
