9ffbfbaacff26303c679121f80779b99cc2e7352
Ray Hidayat <ray.hidayat@gmail.com>
2019-08-17 16:15:53 +1200

    Max width messages

--- src/client/core/userAgent.tsx
+++ src/client/core/userAgent.tsx
@@ -1,6 +1,9 @@
+import queryString from 'query-string';
+const params = queryString.parse(location.search);
+
 export const isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
 export const isEdge = window.navigator.userAgent.indexOf("Edge") > -1;
-export const isMobile = window.navigator.userAgent.toLowerCase().indexOf("mobi") !== -1
+export const isMobile = window.navigator.userAgent.toLowerCase().indexOf("mobi") !== -1 || params["m"] === "1";
 export const isLocal = window.location.host.indexOf("localhost") > -1 || window.location.port.startsWith("777");
 
 console.log(`isSafari=${isSafari} isEdge=${isEdge} isMobile=${isMobile}`);
