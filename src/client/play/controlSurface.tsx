2660dc70bdca49449dd96d083f6569479571da9e
Ray Hidayat <ray.hidayat@gmail.com>
2019-08-19 21:08:27 +1200

    Target at self initially

--- src/client/play/controlSurface.tsx
+++ src/client/play/controlSurface.tsx
@@ -216,7 +216,14 @@ class ControlSurface extends React.PureComponent<Props, State> {
                     }
 
                     if (touchControls(world.ui.buttonBar)) {
-                        world.ui.nextTarget = world.ui.nextTarget || pl.Vec2(0.5, 0.5);
+                        if (!world.ui.nextTarget) {
+                            const hero = world.objects.get(world.ui.myHeroId);
+                            if (hero) {
+                                world.ui.nextTarget = hero.body.getPosition();
+                            } else {
+                                world.ui.nextTarget = pl.Vec2(0.5, 0.5);
+                            }
+                        }
 
                         this.targetSurface = {
                             startWorldPoint: world.ui.nextTarget,
