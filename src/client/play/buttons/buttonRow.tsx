a6bae8b7db5d2a58405991fb97e6ec916999158a
Ray Hidayat <ray.hidayat@gmail.com>
2019-08-31 22:02:34 +1200

    Spread

--- src/client/play/buttons/buttonRow.tsx
+++ src/client/play/buttons/buttonRow.tsx
@@ -13,7 +13,10 @@ interface Props {
 class ButtonRow extends React.PureComponent<Props> {
     render() {
         return <div className="button-panel-row" onMouseEnter={() => this.hover(this.props.label)} onMouseLeave={() => this.hover()}>
-            <Button className="clickable" onClick={this.props.onClick}><span className="button-panel-row-icon"><i className={this.props.icon} /></span><span className="button-panel-row-label">{this.props.label}</span></Button>
+            <Button className="clickable" onClick={this.props.onClick}>
+                <span className="button-panel-row-icon"><i className={this.props.icon} /></span>
+                <span className="button-panel-row-label">{this.props.label}</span>
+            </Button>
         </div>
     }
 
