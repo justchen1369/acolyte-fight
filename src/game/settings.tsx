506b1d796e31478a97b4aa41c10b4edcd761298e
Ray Hidayat <ray.hidayat@gmail.com>
2019-09-02 18:33:06 +1200

    Reverberate stronger

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -2779,7 +2779,7 @@ const bump: Spell = {
             type: "bump",
             passive: true,
             hitInterval: 15,
-            impulse: 0.00015,
+            impulse: 0.000175,
         },
     ],
 
