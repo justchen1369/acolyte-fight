55fe3aa4c7b9cae6ab25e664e08e89596c0e49e3
Ray Hidayat <ray.hidayat@gmail.com>
2019-09-26 21:14:32 +1200

    Void rush/vanish +70%

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -2647,7 +2647,7 @@ const swap: Spell = {
 const voidRush: Spell = {
     id: 'voidRush',
     name: 'Void Rush',
-    description: "For 2.5 seconds, increase movement speed 2x, and also become immune to damage from the void.",
+    description: "For 2.5 seconds, increase movement speed 70%, and also become immune to damage from the void.",
     effects: [
         cleanse,
     ],
@@ -2661,7 +2661,7 @@ const voidRush: Spell = {
     buffs: [
         {
             type: "movement",
-            movementProportion: 2,
+            movementProportion: 1.7,
             maxTicks: 2.5 * TicksPerSecond,
         },
         {
@@ -2691,7 +2691,7 @@ const voidRush: Spell = {
 const vanish: Spell = {
     id: 'vanish',
     name: 'Vanish',
-    description: "Vanish from sight for 2.5 seconds, and also increase movement speed 2x.",
+    description: "Vanish from sight for 2.5 seconds, and also increase movement speed 70%.",
     effects: [
         cleanse,
     ],
@@ -2702,7 +2702,7 @@ const vanish: Spell = {
     throttle: false,
     unlink: true,
     debuff: true,
-    movementProportionWhileChannelling: 2,
+    movementProportionWhileChannelling: 1.7,
     interruptibleAfterTicks: 15,
 
     buffs: [
