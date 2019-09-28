0e7010caf9cd3fa6ceb111ee00b7d0e0477cb61e
Ray Hidayat <ray.hidayat@gmail.com>
2019-09-28 21:34:57 +1200

    Frostsplatter back to 1 second

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1973,7 +1973,7 @@ const iceBomb: Spell = {
                 type: "movement",
                 stack: "iceBomb",
                 movementProportion: 0.1,
-                maxTicks: 1.25 * TicksPerSecond,
+                maxTicks: 1 * TicksPerSecond,
                 against: Alliances.NotFriendly,
                 render: {
                     color: "rgba(64, 255, 255, 1)",
