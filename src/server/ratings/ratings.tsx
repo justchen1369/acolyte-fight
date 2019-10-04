c301fa65625ff8b7af719eeb29c4bc5c323478d2
Ray Hidayat <ray.hidayat@gmail.com>
2019-10-04 23:03:23 +1300

    Factor out rating

--- src/server/ratings/ratings.tsx
+++ src/server/ratings/ratings.tsx
@@ -21,6 +21,22 @@ interface PlayerDelta {
     changes: m.AcoChangeMsg[];
 }
 
+interface UpdateRatingsSnapshot {
+    unix: number;
+    category: string;
+
+    knownPlayers: m.PlayerStatsMsg[];
+    docs: FirebaseFirestore.DocumentSnapshot[];
+
+    initialRatings: Map<string, g.UserRating>,
+    userRatings: Map<string, g.UserRating>,
+
+    loggedInUsers: Set<string>,
+    isRankedLookup: Map<string, boolean>,
+
+    initialDecays: Map<string, db.AcoDecay>,
+}
+
 interface UpdateRatingsResult {
     [userId: string]: PlayerRatingUpdate;
 }
@@ -72,169 +88,87 @@ async function updateRatingsIfNecessary(gameStats: m.GameStatsMsg, isRankedLooku
     const unix = gameStats.unixTimestamp || moment().unix();
 
     const result = await firestore.runTransaction(async (transaction) => {
-        // Load initial data
-        const docs = await transaction.getAll(...knownPlayers.map(p => firestore.collection(Collections.User).doc(p.userId)));
-
-        const initialRatings = new Map<string, g.UserRating>();
-        const userRatings = new Map<string, g.UserRating>();
-        const loggedInUsers = new Set<string>();
-        for (const doc of docs) {
-            const data = doc.data() as db.User;
-
-            const initialRating = statsStorage.dbToUserRating(data, category);
-            initialRatings.set(doc.id, initialRating);
-            userRatings.set(doc.id, {...initialRating});
-
-            const loggedIn = userStorage.dbUserLoggedIn(data)
-            if (loggedIn) {
-                loggedInUsers.add(doc.id);
-            }
-        }
-
-        // Load current decays so they can be updated later
-        const decayDocs = await transaction.getAll(
-            ...knownPlayers.map(p => 
-                firestore.collection(Collections.AcoDecay)
-                .doc(decaying.acoDecayKeyString(decaying.acoDecayKey(p.userId, category, unix))))
-        );
-        const initialDecays = new Map<string, db.AcoDecay>();
-        for (const doc of decayDocs) {
-            if (doc.exists) {
-                const data = doc.data() as db.AcoDecay;
-                initialDecays.set(data.userId, data);
-            }
-        }
+        const snapshot = await readSnapshot(transaction, knownPlayers, isRankedLookup, category, unix);
 
         // Calculate changes
-        const ratingValues = new Map<string, number>();
-        userRatings.forEach((userRating, userId) => {
-            const isRanked = isRankedLookup.get(userId);
-            ratingValues.set(userId, isRanked ? userRating.aco : userRating.acoUnranked);
-        });
-        const deltas = calculateNewAcoRatings(ratingValues, knownPlayers, category);
+        const deltas = calculateDeltas(snapshot);
 
         // Apply changes
-        const result: UpdateRatingsResult = {};
-        for (const playerDelta of wu(deltas.values()).toArray()) {
-            const isRanked = isRankedLookup.get(playerDelta.userId);
-            const initialRating = initialRatings.get(playerDelta.userId);
-            const selfRating = userRatings.get(playerDelta.userId);
-            if (!selfRating) {
-                continue;
-            }
-
-            if (isRanked) {
-                const changes = [...playerDelta.changes];
-
-                const initialExposure = statsStorage.calculateAcoExposure(selfRating.aco, selfRating.acoGames, selfRating.acoDeflate);
-
-                for (const change of playerDelta.changes) {
-                    selfRating.aco += change.delta;
-
-                    if (selfRating.acoUnranked <= selfRating.aco) {
-                        // Don't allow the unranked rating to get too low because then it can be used to destroy other people's rankings
-                        selfRating.acoUnranked += change.delta;
-                    }
-                }
-
-                if (selfRating.acoDeflate > 0) {
-                    const inflate = Math.min(selfRating.acoDeflate, constants.Placements.AcoInflatePerGame);
-                    selfRating.acoDeflate -= inflate;
-
-                    changes.push({ delta: inflate });
-                }
-
-                ++selfRating.acoGames;
-
-                const finalExposure = statsStorage.calculateAcoExposure(selfRating.aco, selfRating.acoGames, selfRating.acoDeflate);
-
-                result[playerDelta.userId] = {
-                    isRanked,
-                    initialNumGames: initialRating.numGames,
-                    initialAco: initialRating.aco,
-                    initialAcoGames: initialRating.acoGames,
-                    initialAcoDeflate: initialRating.acoDeflate,
-                    initialAcoExposure: initialExposure,
-                    acoChanges: changes,
-                    acoDelta: finalExposure - initialExposure,
-                };
-            } else {
-                for (const change of playerDelta.changes) {
-                    selfRating.acoUnranked += change.delta;
-                }
-
-                result[playerDelta.userId] = {
-                    isRanked,
-                    initialNumGames: initialRating.numGames,
-                    initialAco: initialRating.acoUnranked,
-                    initialAcoDeflate: null,
-                    initialAcoGames: null,
-                    initialAcoExposure: null,
-                    acoChanges: [],
-                    acoDelta: null,
-                };
-            }
-        }
-
-        // Update stats
-        for (const player of knownPlayers) {
-            const isRanked = isRankedLookup.get(player.userId);
-            if (isRanked) {
-                const userRating = userRatings.get(player.userId);
-                const isWinner = player.rank === constants.Placements.Rank1;
-                calculateNewStats(userRating, player, isWinner);
-            }
-        }
-
-        // Increment num games
-        userRatings.forEach(userRating => {
-            ++userRating.numGames;
-        });
+        const result: UpdateRatingsResult = applyDeltas(deltas, snapshot);
+        updateStats(snapshot);
+        incrementNumGames(snapshot);
 
         // Write update
-        for (const doc of docs) {
-            const loggedIn = loggedInUsers.has(doc.id);
-            const dbUserRating = statsStorage.userRatingToDb(userRatings.get(doc.id), loggedIn);
-
-            // Save rating
-            const delta: Partial<db.User> = {
-                ratings: { [category]: dbUserRating },
-            };
-            transaction.update(doc.ref, delta);
-
-            // Save rating to history
-            if (loggedIn) {
-                const unixDate = unixDateFromTimestamp(gameStats.unixTimestamp);
-                const historyItem: db.UserRatingHistoryItem = {
-                    unixDate,
-                    ratings: { [category]: dbUserRating },
-                };
-                const historyId = moment.unix(unixDate).format("YYYY-MM-DD");
-                transaction.set(doc.ref.collection(Collections.UserRatingHistory).doc(historyId), historyItem);
-            }
-        }
-
-        // Prepare for decay
-        for (const player of knownPlayers) {
-            const initialRating = initialRatings.get(player.userId);
-            const userRating = userRatings.get(player.userId);
-
-            // Update aco decay
-            const decay = initialDecays.get(player.userId) || decaying.initAcoDecay(player.userId, category, unix);
-
-            decay.acoDelta += userRating.aco - initialRating.aco;
-            decay.acoGamesDelta += userRating.acoGames - initialRating.acoGames;
-
-            if (decay.acoDelta || decay.acoGamesDelta) {
-                const key = decaying.acoDecayKeyString(decay);
-                transaction.set(firestore.collection(Collections.AcoDecay).doc(key), decay);
-            }
-        }
+        await writeUsers(snapshot, transaction);
+        await writeDecay(snapshot, transaction);
 
         return result;
     });
 
     return result;
+}
+
+async function readSnapshot(
+    transaction: FirebaseFirestore.Transaction,
+    knownPlayers: m.PlayerStatsMsg[],
+    isRankedLookup: Map<string, boolean>,
+    category: string,
+    unix: number): Promise<UpdateRatingsSnapshot> {
+
+    const firestore = getFirestore();
+
+    // Load initial data
+    const docs = await transaction.getAll(...knownPlayers.map(p => firestore.collection(Collections.User).doc(p.userId)));
+
+    const initialRatings = new Map<string, g.UserRating>();
+    const userRatings = new Map<string, g.UserRating>();
+    const loggedInUsers = new Set<string>();
+    for (const doc of docs) {
+        const data = doc.data() as db.User;
+
+        const initialRating = statsStorage.dbToUserRating(data, category);
+        initialRatings.set(doc.id, initialRating);
+        userRatings.set(doc.id, {...initialRating});
+
+        const loggedIn = userStorage.dbUserLoggedIn(data)
+        if (loggedIn) {
+            loggedInUsers.add(doc.id);
+        }
+    }
+
+    // Load current decays so they can be updated later
+    const initialDecays = await loadDecays(transaction, knownPlayers, category, unix);
+
+    return {
+        category,
+        unix,
+        knownPlayers,
+        docs,
+        initialRatings,
+        userRatings,
+        loggedInUsers,
+        isRankedLookup,
+        initialDecays,
+    };
+}
+
+async function loadDecays(transaction: FirebaseFirestore.Transaction, knownPlayers: m.PlayerStatsMsg[], category: string, unix: number) {
+    const firestore = getFirestore();
+
+    const decayDocs = await transaction.getAll(
+        ...knownPlayers.map(p => 
+            firestore.collection(Collections.AcoDecay)
+            .doc(decaying.acoDecayKeyString(decaying.acoDecayKey(p.userId, category, unix))))
+    );
+    const initialDecays = new Map<string, db.AcoDecay>();
+    for (const doc of decayDocs) {
+        if (doc.exists) {
+            const data = doc.data() as db.AcoDecay;
+            initialDecays.set(data.userId, data);
+        }
+    }
+
+    return initialDecays;
 }
 
 function unixDateFromTimestamp(unixTimestamp: number): number {
@@ -261,6 +195,16 @@ function applyRatingDeltas(gameStats: m.GameStatsMsg, updateResult: UpdateRating
             }
         }
     }
+}
+
+function calculateDeltas(snapshot: UpdateRatingsSnapshot) {
+    const ratingValues = new Map<string, number>();
+    snapshot.userRatings.forEach((userRating, userId) => {
+        const isRanked = snapshot.isRankedLookup.get(userId);
+        ratingValues.set(userId, isRanked ? userRating.aco : userRating.acoUnranked);
+    });
+    const deltas = calculateNewAcoRatings(ratingValues, snapshot.knownPlayers, snapshot.category);
+    return deltas;
 }
 
 function calculateNewAcoRatings(ratingValues: Map<string, number>, players: m.PlayerStatsMsg[], category: string): Map<string, PlayerDelta> {
@@ -338,8 +282,99 @@ function calculateNewAcoRatings(ratingValues: Map<string, number>, players: m.Pl
     return deltas;
 }
 
+function applyDeltas(
+    deltas: Map<string, PlayerDelta>,
+    snapshot: UpdateRatingsSnapshot) {
+
+    const result: UpdateRatingsResult = {};
+    for (const playerDelta of wu(deltas.values()).toArray()) {
+        const isRanked = snapshot.isRankedLookup.get(playerDelta.userId);
+        const initialRating = snapshot.initialRatings.get(playerDelta.userId);
+        const selfRating = snapshot.userRatings.get(playerDelta.userId);
+
+        if (selfRating) {
+            result[playerDelta.userId] =
+                isRanked
+                ? applyRanked(playerDelta, initialRating, selfRating)
+                : applyUnranked(playerDelta, initialRating, selfRating);
+        }
+    }
+    return result;
+}
+
+function applyRanked(playerDelta: PlayerDelta, initialRating: g.UserRating, selfRating: g.UserRating): PlayerRatingUpdate {
+    const changes = [...playerDelta.changes];
+
+    const initialExposure = statsStorage.calculateAcoExposure(selfRating.aco, selfRating.acoGames, selfRating.acoDeflate);
+
+    for (const change of playerDelta.changes) {
+        selfRating.aco += change.delta;
+
+        if (selfRating.acoUnranked <= selfRating.aco) {
+            // Don't allow the unranked rating to get too low because then it can be used to destroy other people's rankings
+            selfRating.acoUnranked += change.delta;
+        }
+    }
+
+    if (selfRating.acoDeflate > 0) {
+        const inflate = Math.min(selfRating.acoDeflate, constants.Placements.AcoInflatePerGame);
+        selfRating.acoDeflate -= inflate;
+
+        changes.push({ delta: inflate });
+    }
+
+    ++selfRating.acoGames;
+
+    const finalExposure = statsStorage.calculateAcoExposure(selfRating.aco, selfRating.acoGames, selfRating.acoDeflate);
+
+    return {
+        isRanked: false,
+        initialNumGames: initialRating.numGames,
+        initialAco: initialRating.aco,
+        initialAcoGames: initialRating.acoGames,
+        initialAcoDeflate: initialRating.acoDeflate,
+        initialAcoExposure: initialExposure,
+        acoChanges: changes,
+        acoDelta: finalExposure - initialExposure,
+    };
+}
+
+function applyUnranked(playerDelta: PlayerDelta, initialRating: g.UserRating, selfRating: g.UserRating): PlayerRatingUpdate {
+    for (const change of playerDelta.changes) {
+        selfRating.acoUnranked += change.delta;
+    }
+
+    return {
+        isRanked: false,
+        initialNumGames: initialRating.numGames,
+        initialAco: initialRating.acoUnranked,
+        initialAcoDeflate: null,
+        initialAcoGames: null,
+        initialAcoExposure: null,
+        acoChanges: [],
+        acoDelta: null,
+    };
+}
+
 function calculateAverageRating(ratings: number[]) {
     return _.mean(ratings);
+}
+
+function updateStats(snapshot: UpdateRatingsSnapshot) {
+    for (const player of snapshot.knownPlayers) {
+        const isRanked = snapshot.isRankedLookup.get(player.userId);
+        if (isRanked) {
+            const userRating = snapshot.userRatings.get(player.userId);
+            const isWinner = player.rank === constants.Placements.Rank1;
+            calculateNewStats(userRating, player, isWinner);
+        }
+    }
+}
+
+function incrementNumGames(snapshot: UpdateRatingsSnapshot) {
+    snapshot.userRatings.forEach(userRating => {
+        ++userRating.numGames;
+    });
 }
 
 function calculateNewStats(userRating: g.UserRating, player: m.PlayerStatsMsg, isWinner: boolean) {
@@ -351,4 +386,49 @@ function calculateNewStats(userRating: g.UserRating, player: m.PlayerStatsMsg, i
 
 function incrementAverage(current: number, count: number, newValue: number) {
     return (current * count + newValue) / (count + 1);
+}
+
+async function writeUsers(snapshot: UpdateRatingsSnapshot, transaction: FirebaseFirestore.Transaction) {
+    for (const doc of snapshot.docs) {
+        const loggedIn = snapshot.loggedInUsers.has(doc.id);
+        const dbUserRating = statsStorage.userRatingToDb(snapshot.userRatings.get(doc.id), loggedIn);
+
+        // Save rating
+        const delta: Partial<db.User> = {
+            ratings: { [snapshot.category]: dbUserRating },
+        };
+        transaction.update(doc.ref, delta);
+
+        // Save rating to history
+        if (loggedIn) {
+            const unixDate = unixDateFromTimestamp(snapshot.unix);
+            const historyItem: db.UserRatingHistoryItem = {
+                unixDate,
+                ratings: { [snapshot.category]: dbUserRating },
+            };
+            const historyId = moment.unix(unixDate).format("YYYY-MM-DD");
+            transaction.set(doc.ref.collection(Collections.UserRatingHistory).doc(historyId), historyItem);
+        }
+    }
+}
+
+async function writeDecay(snapshot: UpdateRatingsSnapshot, transaction: FirebaseFirestore.Transaction) {
+    const firestore = getFirestore();
+
+    // Prepare for decay
+    for (const player of snapshot.knownPlayers) {
+        const initialRating = snapshot.initialRatings.get(player.userId);
+        const userRating = snapshot.userRatings.get(player.userId);
+
+        // Update aco decay
+        const decay = snapshot.initialDecays.get(player.userId) || decaying.initAcoDecay(player.userId, snapshot.category, snapshot.unix);
+
+        decay.acoDelta += userRating.aco - initialRating.aco;
+        decay.acoGamesDelta += userRating.acoGames - initialRating.acoGames;
+
+        if (decay.acoDelta || decay.acoGamesDelta) {
+            const key = decaying.acoDecayKeyString(decay);
+            transaction.set(firestore.collection(Collections.AcoDecay).doc(key), decay);
+        }
+    }
 }
