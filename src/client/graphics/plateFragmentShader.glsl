a8c13205b9a51cd840263502341bb2b544eab9bf
Ray Hidayat <ray.hidayat@gmail.com>
2019-10-19 19:43:17 +1300

    No hex

--- src/client/graphics/plateFragmentShader.glsl
+++ src/client/graphics/plateFragmentShader.glsl
@@ -11,41 +11,6 @@ varying vec4 v_color;
 varying vec4 v_strokeColor;
 varying vec2 v_range;
 
-#define HexColSize 10.0
-#define HexRowSize 7.0
-#define HexTopProportion 0.577
-#define HexHalfProportion (HexTopProportion / 2.0)
-#define Hex1 (0.5 - HexHalfProportion)
-
-bool isHex(vec2 p) {
-	float hexRowSize = u_pixel * HexRowSize;
-	float hexColSize = u_pixel * HexColSize;
-
-	float row = mod(p.y, hexRowSize * 2.0) / (hexRowSize * 2.0); // Two quads per hex down so that it tesselates
-	float col = mod(p.x, hexColSize) / (hexColSize); // One squad per hex across so that it tesselates
-
-	float halfCol = 1.0 - 2.0 * abs(col - 0.5); // 0.0 (side col) - 1.0 (middle col)
-	float hexRow1 = mix(Hex1 / 2.0, -Hex1 / 2.0, halfCol);
-	float hexRow2 = mix((1.0 - Hex1) / 2.0, (1.0 + Hex1) / 2.0, halfCol);
-
-	float rowDist = abs(row - hexRow1); // Check against first
-	rowDist = min(rowDist, abs(row - (hexRow1 + 1.0))); // Check against first wrapped
-	rowDist = min(rowDist, abs(row - hexRow2)); // Check against second
-
-	float pixel = 0.5 / HexRowSize; 
-	if (rowDist <= pixel) {
-		return true;
-	}
-
-	if (abs(col) <= pixel) {
-		return hexRow1 <= row && row <= hexRow2;
-	} else if (abs(col - 0.5) <= pixel) {
-		return hexRow2 <= row && row <= (hexRow1 + 1.0);
-	}
-
-	return false;
-}
-
 void main() {
 	vec4 color = vec4(v_color);
 
@@ -61,10 +26,6 @@ void main() {
 		// Smooth
 		float alpha = smoothstep(0.0, u_pixel, outside);
 		color = mix(color, strokeColor, alpha);
-	} else {
-		if (isHex(v_draw)) {
-			color.rgb *= 0.95;
-		}
 	}
 
 	if (radius > strokeRange) {
