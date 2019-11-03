fff2dacec10fc7330de1c75d44c2245dd0ba3c3c
Ray Hidayat <ray.hidayat@gmail.com>
2019-11-03 14:24:11 +1300

    Shields back to 0.25

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -2188,7 +2188,7 @@ const saber: Spell = {
     speedMultiplier: 2,
     maxSpeed: 0.75,
     maxTurnRatePerTickInRevs: 0.1,
-    damageMultiplier: 0.5,
+    damageMultiplier: 0.25,
 
     angleOffsetsInRevs: [0],
     width: Pixel,
@@ -2249,7 +2249,7 @@ const dualSaber: Spell = {
     speedMultiplier: 2,
     maxSpeed: 0.75,
     maxTurnRatePerTickInRevs: 0.1,
-    damageMultiplier: 0.5,
+    damageMultiplier: 0.25,
 
     angleOffsetsInRevs: [-0.25, 0.25],
     width: Pixel,
@@ -2333,7 +2333,7 @@ const shield: Spell = {
     radius: Hero.Radius * 1.8,
     takesOwnership: true,
     blocksTeleporters: false,
-    damageMultiplier: 0.5,
+    damageMultiplier: 0.25,
 
     icon: "shieldReflect",
 
@@ -2437,7 +2437,7 @@ const icewall: Spell = {
     throttle: false,
     takesOwnership: true,
     blocksTeleporters: true,
-    damageMultiplier: 0.5,
+    damageMultiplier: 0.25,
 
     length: 0.005,
     width: 0.15,
