684bb827892eea8d6992fce59397cd4daae57f52
Ray Hidayat <ray.hidayat@gmail.com>
2019-11-11 20:19:49 +1300

    Atlas always hidden

--- src/client/play/canvasPanel.tsx
+++ src/client/play/canvasPanel.tsx
@@ -197,7 +197,6 @@ class CanvasPanel extends React.PureComponent<Props, State> {
 
                 <canvas
                     id="atlas" ref={c => this.canvasStack.atlas = c} className="atlas"
-                    style={{ visibility: 'hidden' }}
                     width={Atlas.Width} height={Atlas.Height}
                 />
 
