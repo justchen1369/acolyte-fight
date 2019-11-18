815d5be2a73d6b37885e22e0f9f0c8a08663b68e
Ray Hidayat <ray.hidayat@gmail.com>
2019-11-18 19:40:14 +1300

    Proceed replays to start of action

--- src/client/core/processor.tsx
+++ src/client/core/processor.tsx
@@ -24,7 +24,7 @@ export function connectToWorld(world: w.World, data: m.HeroMsg) {
 }
 
 export function isStartGameTick(tickData: m.TickMsg) {
-	return tickData.c && tickData.c.some(x => x.type === m.ActionType.CloseGame);
+	return tickData.c && tickData.c.some(x => x.type === m.ActionType.CloseGame && x.waitPeriod <= 0);
 }
 
 export function applyTick(tickData: m.TickMsg, world: w.World) {
