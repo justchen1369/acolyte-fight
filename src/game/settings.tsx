d8fb4e309090d537b332694b07b29d953059986b
Ray Hidayat <ray.hidayat@gmail.com>
2019-12-01 10:16:46 +1300

    Spirit bomb start with 50% not 33% detonation radius

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1123,7 +1123,7 @@ const rocket: Spell = {
         shieldTakesOwnership: false,
 
         partialDetonateRadius: {
-            initialMultiplier: 0.33,
+            initialMultiplier: 0.5,
             ticks: 1 * TicksPerSecond,
         },
 
