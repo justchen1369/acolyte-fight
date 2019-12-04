80711285c16410b121b0f64716064db44a640279
Ray Hidayat <ray.hidayat@gmail.com>
2019-12-04 21:24:23 +1300

    YouTube link on social bar

--- src/client/controls/socialBar.tsx
+++ src/client/controls/socialBar.tsx
@@ -40,7 +40,7 @@ class SocialBar extends React.PureComponent<Props, State> {
     private renderSocialBar() {
         return <div className="social-bar">
             <a href="https://facebook.com/acolytefight" target="_blank" title="Facebook"><i className="fab fa-facebook-square" /></a>
-            <a href="http://twitter.com/acolytefight" target="_blank" title="Twitter"><i className="fab fa-twitter-square" /></a>
+            <a href="https://www.youtube.com/channel/UCJr11iCM_aigs5mCqhF_H2g" target="_blank" title="Twitter"><i className="fab fa-youtube-square" /></a>
             <a href="https://discord.gg/sZvgpZk" target="_blank" title="Chat on Discord!"><i className="fab fa-discord" /></a>
         </div>
     }
