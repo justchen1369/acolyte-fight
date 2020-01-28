de525b714d213b328a65f91c251ab0d66fe59eb7
Ray Hidayat <ray.hidayat@gmail.com>
2020-01-29 00:34:28 +0100

    Few more special characters

--- src/shared/sanitize.tsx
+++ src/shared/sanitize.tsx
@@ -2,7 +2,7 @@ export const MaxPlayerNameLength = 24;
 
 export function sanitizeName(input: string) {
     let name = input || "";
-    name = name.replace(/[^A-Za-z0-9_-]/g, '');
+    name = name.replace(/[^A-Za-z0-9_\-\.\+@]/g, '');
     if (name.length > MaxPlayerNameLength) {
         name = name.substring(0, MaxPlayerNameLength);
     }
@@ -10,5 +10,5 @@ export function sanitizeName(input: string) {
 }
 
 export function validName(name: string) {
-    return name && name.trim().length > 0;
+    return name && name.trim().length > 0 && /[A-Za-z0-9]/.test(name);
 }
